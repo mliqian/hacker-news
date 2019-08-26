34a3193b033123e868a40808e2d3b45f583068d5
mliqian <qianqianio@qq.com>
2019-08-26 16:02:30 +0800

    Add style related

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import News from "./News";
+import "./index.css";
 
 function Nav() {
   let theme = "light";
