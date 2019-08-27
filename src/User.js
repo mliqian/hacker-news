c34d27ed14c021a918defc32fece37d7fb9861e6
mliqian <qianqianio@qq.com>
2019-08-27 18:06:25 +0800

    Fetch user data

--- src/User.js
+++ src/User.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import queryString from "query-string";
+import Loading from "./Loading";
+import { formatUserCreatedTime } from "./utils";
 
+/*
+User: {
+  "about" : "This is a test",
+  created: 1334265808
+  id: "ingve"
+  karma: 108620
+  submitted: [20803091, 20802509, ...]
+}
+*/
 function User({ location }) {
+  let [user, setUser] = useState(null);
   let { id } = queryString.parse(location.search);
-  console.log("变量输出 user: ", id);
-  return <div>User</div>;
+  let url = `https://hacker-news.firebaseio.com/v0/user/${id}.json`;
+  useEffect(
+    function() {
+      fetch(url)
+        .then(res => res.json())
+        .then(setUser);
+    },
+    [url]
+  );
+
+  if (!user) {
+    return <Loading />;
+  }
+  return (
+    <table className="user-table">
+      <tbody>
+        <tr>
+          <td>User:</td>
+          <td>{user.id}</td>
+        </tr>
+        <tr>
+          <td>Created:</td>
+          <td>{formatUserCreatedTime(user.created)}</td>
+        </tr>
+        <tr>
+          <td>karma:</td>
+          <td>{user.karma}</td>
+        </tr>
+        <tr>
+          <td>about:</td>
+          <td>{user.about}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 User.propTypes = {
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,36 @@
-export function formatUnixTime(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+const months = [
+  "Jenuary",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+function parseUnixTime(milliTimestamp) {
+  const date = new Date(milliTimestamp * 1000);
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes()
+  };
+}
+export function formatUnixTime(milliTimestamp) {
+  const { year, month, day, hour, minute } = parseUnixTime(milliTimestamp);
 
   return `${month}/${day}/${year}, ${hour}:${minute}`;
+}
+
+export function formatUserCreatedTime(milliTimestamp) {
+  const { year, month, day } = parseUnixTime(milliTimestamp);
+
+  return `${months[month - 1]} ${day}, ${year}`;
 }
