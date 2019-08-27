0b28b74df561df45a70fe66f933ca28a50799359
mliqian <qianqianio@qq.com>
2019-08-27 17:29:30 +0800

    Add query-string

--- src/User.js
+++ src/User.js
@@ -1,7 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
+import queryString from "query-string";
 
-function User() {
+function User({ location }) {
+  let { id } = queryString.parse(location.search);
+  console.log("变量输出 user: ", id);
   return <div>User</div>;
 }
+
+User.propTypes = {
+  location: PropTypes.shape({
+    search: PropTypes.string
+  }).isRequired
+};
 
 export default User;
